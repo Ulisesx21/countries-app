82acf3a7c7b4a802053282e32ad18ef5377173ab
Ulisesx21 <75042766+Ulisesx21@users.noreply.github.com>
2022-08-18 18:30:51 -0300

    -

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
 
 
@@ -9,8 +9,8 @@ import { BrowserRouter } from "react-router-dom"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
