08060959030109bdc9fe5df28a83fdabea1a278f
Ulisesx21 <75042766+Ulisesx21@users.noreply.github.com>
2022-08-24 06:58:25 -0300

    -

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import "../css/Header.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faMoon,faSun } from '@fortawesome/free-solid-svg-icons'
 import { useState } from "react"
 
 
@@ -9,6 +9,7 @@ export default function Header({ mode }){
     let [lmode, setLmode] = useState(true)
 
     const Moon = <FontAwesomeIcon icon={faMoon} className={`${lmode ? "i-moon" : "i-moon-D"}`}/>
+    const Sun = <FontAwesomeIcon icon={faSun} className={`${lmode ? "i-moon" : "i-moon-D"}`}/>
 
     function handleMode(){
         setLmode(!lmode)
@@ -22,8 +23,8 @@ export default function Header({ mode }){
             </div>
             <div className="mode">
                 <button className={`${lmode ? "btn-mode" : "btn-mode-D"}`} onClick={handleMode}>
-                    {Moon}
-                    Dark Mode
+                    {lmode ? Moon : Sun}
+                    {lmode ? "Dark Mode" : "Light Mode"}
                 </button>
             </div>
         </header>
